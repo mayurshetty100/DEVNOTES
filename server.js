54d2447b4d20d98bd33d548478764c214139ec37
mayurshetty100 <mayurshetty9743@gmail.com>
2026-02-12 20:02:02 +0530

    checked the post request and json parsing of response in the backend

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express=require('express');
 
 const app=express();
 
+// middleware to enable json parsing 
+app.use(express.json());
+
 app.get('/',(req,res)=>{
     res.send("devnotes api running...");
+});
+
+app.post('/test',(req,res)=>{
+   res.json({recievedData:req.body});
 });
 
 app.listen(3000,()=>{
