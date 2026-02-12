57dce576c3bde965ff61e684e426fc75222e49c6
mayurshetty100 <mayurshetty9743@gmail.com>
2026-02-12 20:06:04 +0530

    added the .env file and secured the port number

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express=require('express');
 
 const app=express();
 
+//require the dotenv to access the values from the .env file
+require('dotenv').config();
+const PORT=process.env.PORT || 3000;
 // middleware to enable json parsing 
 app.use(express.json());
 
@@ -13,6 +16,6 @@ app.post('/test',(req,res)=>{
    res.json({recievedData:req.body});
 });
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log('server runnning on port 3000');
 });
