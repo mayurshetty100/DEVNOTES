3d26b786227147aa8af099743b15e1556b6d2efb
mayurshetty100 <mayurshetty9743@gmail.com>
2026-02-13 21:14:06 +0530

    added bcrypt for hashed password

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -1,5 +1,7 @@
 const User = require('../models/User');
 
+//import bcrypt for password hashing
+const bcrypt=require('bcryptjs');
 
 // registering new user
 
@@ -14,10 +16,12 @@ const registerUser= async (req,res)=>{
         if(existingUser){
             return res.status(400).json({message:"User with this email already exists"});
         }
-
+        //hash the password before saving to the database
+        const salt=await bcrypt.genSalt(10);
+        const hashedPassword=await bcrypt.hash(passoword,salt);
         //create new user
         const user=await User.create({
-            name,email,password
+            name,email,password:hashedPassword
         });
         
         res.status(201).json({message:"User registered successfully",userId:user._id});
