921e99768ed5b965629e904dcd3988c50eaf4186
mayurshetty100 <mayurshetty9743@gmail.com>
2026-02-18 20:01:26 +0530

    correcting the post request of noteRoutes.js for creating new notes

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -7,18 +7,11 @@ const router=express.Router();
 //import auth middleware
 const protect=require('../middlewares/authMiddleware');
 
-// GET /api/notes protected route
-
-router.get('/',protect,(req,res)=>{
-    res.json({
-        message:"You accessed the protected notes route",
-        user:req.user
-    });
-});
-
 // import createNote controller
 const {createNote}=require('../controllers/notecontroller');
 
+//POST api/notes protected route to create a new note
+router.post("/",protect,createNote);
 // GET /api/notes protected route to create a new note
 router.get("/",protect,(req,res)=>{
     res.json({
