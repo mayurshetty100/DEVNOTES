61dcaeafb85130926f808a9bf12749d8c7392079
mayurshetty100 <mayurshetty9743@gmail.com>
2026-02-25 21:38:30 +0530

    added helmet middleware to set http headers to protect against common attacks

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app=express();
 //import cors to enable cross-origin resource sharing
 const cors=require('cors');
 
+//require helmet to set security headers
+const helmet=require('helmet');
 
 //require the dotenv to access the values from the .env file
 require('dotenv').config();
@@ -16,7 +18,9 @@ const connectDB = require("./config/db");
 //connect db 
 connectDB();
 app.use(express.json());
-app.use(cors());
+app.use(cors()); // here order matters because express executes the middlewares from top to bottom so order matters but for cors it is flexible
+// the above is industry standard with body parsing first followed by cors but it can be in any order as long as they are before the routes that need them. for example if we have a route that needs to parse json and we put the cors middleware after that route then it will not work because the request will not be able to reach the cors middleware to enable cross-origin resource sharing. so it is important to place the middlewares in the correct order to ensure that they work as expected.
+app.use(helmet());// here order matters because helmet sets security headers and it should be placed before the routes to ensure that the headers are set for all the routes. if we place it after the routes then the headers will not be set for those routes and it will not provide the intended security benefits. so it is important to place helmet before the routes to ensure that it works as expected.
 
 app.get('/',(req,res)=>{
     res.send("devnotes api running...");
