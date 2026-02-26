7efe3dd636cddffacb0560a9596bdb83f1ff6f3d
mayurshetty100 <mayurshetty9743@gmail.com>
2026-02-26 22:40:50 +0530

    setup react frontend

--- middlewares/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-/* wrapper for async errors
-eliminates the need for try-catch blocks in async functions (controllers)
-*/
-const asyncHandler=(fn)=> (req,res,next)=>{
-    Promise.resolve(fn(req,res,next)).catch(next);
-};
-
-module.exports=asyncHandler;
